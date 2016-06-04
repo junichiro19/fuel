22586b231543a49fcc95e35b906aa7075d5f25c7
jun <okjs19@gmail.com>
2016-06-04 06:04:31 +0000

    third commit

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,25 +1,9 @@
 class MapsController < ApplicationController
-    before_action :set_map, only: [:show, :edit, :update, :destroy]
-    
-    
+ 
+
     def index
-     @map = Map.all
+   
     end
     
-    def new
-     @map = Map.new
-    end
-    
-    def create
-     @map = Map.new(set_map)
-     @map.save
-     redirect_to show_path 
-    end
-    
-    private
-    
-    def set_map
-        params.require(:map).permit(:title, :address, :latitude, :longitude)
-    end
-    
+   
 end
